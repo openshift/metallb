9d6ed017396be97d5df66530a99a22b069420398
Federico Paolinelli <fpaoline@redhat.com>
2022-06-14 14:58:40 +0200

    Patch also the webhook server image
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- openshift-ci/deploy_metallb.sh
+++ openshift-ci/deploy_metallb.sh
@@ -25,6 +25,8 @@ go install -mod='' github.com/mikefarah/yq/v4@v4.13.3
 yq e --inplace '.spec.template.spec.containers[0].env[] |= select (.name=="SPEAKER_IMAGE").value|="'${METALLB_IMAGE_BASE}':'${METALLB_IMAGE_TAG}'"' ${metallb_dir}/metallb-operator-deploy/controller_manager_patch.yaml
 yq e --inplace '.spec.template.spec.containers[0].env[] |= select (.name=="CONTROLLER_IMAGE").value|="'${METALLB_IMAGE_BASE}':'${METALLB_IMAGE_TAG}'"' ${metallb_dir}/metallb-operator-deploy/controller_manager_patch.yaml
 yq e --inplace '.spec.template.spec.containers[0].env[] |= select (.name=="FRR_IMAGE").value|="'${METALLB_IMAGE_BASE}':'${FRR_IMAGE_TAG}'"' ${metallb_dir}/metallb-operator-deploy/controller_manager_patch.yaml
+yq e --inplace '.spec.template.spec.containers[0].image |="'${METALLB_IMAGE_BASE}':'${METALLB_IMAGE_TAG}'"' ${metallb_dir}/metallb-operator-deploy/webhook_server_patch.yaml
+
 
 PATH="${GOPATH}:${PATH}" ENABLE_OPERATOR_WEBHOOK=true KUSTOMIZE_DEPLOY_DIR="../metallb-operator-deploy" IMG="${METALLB_IMAGE_BASE}:${METALLB_OPERATOR_IMAGE_TAG}" make deploy
 
