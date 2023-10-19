8d07c9f4958b826156e6c2d4cbf2b072755a42aa
Federico Paolinelli <fpaoline@redhat.com>
2023-10-19 09:59:42 +0200

    Controller: disable http2 for the webhook endpoint
    This is optional, and should remediate the http2 fast reset bug.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- internal/k8s/k8s.go
+++ internal/k8s/k8s.go
@@ -5,6 +5,7 @@ package k8s // import "go.universe.tf/metallb/internal/k8s"
 import (
 	"context"
 	"crypto/rand"
+	"crypto/tls"
 	"encoding/base64"
 	"fmt"
 	"net"
@@ -112,6 +113,7 @@ type Config struct {
 	CertDir             string
 	CertServiceName     string
 	LoadBalancerClass   string
+	WebhookWithHTTP2    bool
 	Listener
 }
 
@@ -129,6 +131,7 @@ func New(cfg *Config) (*Client, error) {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
 		Port:               9443, // TODO port only with controller, for webhooks
+		TLSOpts:            tlsOpts(cfg.WebhookWithHTTP2),
 		LeaderElection:     false,
 		MetricsBindAddress: "0", // Disable metrics endpoint of controller manager
 		NewCache: cache.BuilderWithOptions(cache.Options{
@@ -146,6 +149,7 @@ func New(cfg *Config) (*Client, error) {
 			},
 		}),
 	})
+
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
@@ -422,4 +426,14 @@ func UseEndpointSlices(kubeClient kubernetes.Interface) bool {
 		return false
 	}
 	return true
+}
+
+func tlsOpts(withHTTP2 bool) []func(config *tls.Config) {
+	disableHTTP2 := func(c *tls.Config) {
+		if withHTTP2 {
+			return
+		}
+		c.NextProtos = []string{"http/1.1"}
+	}
+	return []func(config *tls.Config){disableHTTP2}
 }
