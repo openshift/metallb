7243fd7641962098a800519673f37a0ddfceec32
cong <q1875486458@gmail.com>
2024-01-12 19:39:00 +0800

    Metrics: add ipv4/6 addresses_in_use_total and addresses_total
    Signed-off-by: cong <q1875486458@gmail.com>

--- internal/allocator/allocator_test.go
+++ internal/allocator/allocator_test.go
@@ -1945,6 +1945,8 @@ func TestPoolCount(t *testing.T) {
 		desc string
 		pool *config.Pool
 		want int64
+		ipv4 int64
+		ipv6 int64
 	}{
 		{
 			desc: "BGP /24",
@@ -1952,6 +1954,8 @@ func TestPoolCount(t *testing.T) {
 				CIDR: []*net.IPNet{ipnet("1.2.3.0/24")},
 			},
 			want: 256,
+			ipv4: 256,
+			ipv6: 0,
 		},
 		{
 			desc: "BGP /24 and /25",
@@ -1959,6 +1963,8 @@ func TestPoolCount(t *testing.T) {
 				CIDR: []*net.IPNet{ipnet("1.2.3.0/24"), ipnet("2.3.4.128/25")},
 			},
 			want: 384,
+			ipv4: 384,
+			ipv6: 0,
 		},
 		{
 			desc: "BGP /24 and /25, no buggy IPs",
@@ -1967,6 +1973,8 @@ func TestPoolCount(t *testing.T) {
 				AvoidBuggyIPs: true,
 			},
 			want: 381,
+			ipv4: 381,
+			ipv6: 0,
 		},
 		{
 			desc: "BGP a BIG ipv6 range",
@@ -1975,13 +1983,31 @@ func TestPoolCount(t *testing.T) {
 				AvoidBuggyIPs: true,
 			},
 			want: math.MaxInt64,
+			ipv4: 381,
+			ipv6: math.MaxInt64,
+		},
+		{
+			desc: "ipv4 and ipv6 range",
+			pool: &config.Pool{
+				CIDR:          []*net.IPNet{ipnet("1.2.3.0/31"), ipnet("1000::/127")},
+				AvoidBuggyIPs: true,
+			},
+			want: 3,
+			ipv4: 1,
+			ipv6: 2,
 		},
 	}
 
 	for _, test := range tests {
-		got := poolCount(test.pool)
-		if test.want != got {
-			t.Errorf("%q: wrong pool count, want %d, got %d", test.desc, test.want, got)
+		total, ipv4, ipv6 := poolCount(test.pool)
+		if test.want != total {
+			t.Errorf("%q: wrong pool total count, want %d, got %d", test.desc, test.want, total)
+		}
+		if test.ipv4 != ipv4 {
+			t.Errorf("%q: wrong pool ipv4 count, want %d, got %d", test.desc, test.ipv4, ipv4)
+		}
+		if test.ipv6 != ipv6 {
+			t.Errorf("%q: wrong pool ipv6 count, want %d, got %d", test.desc, test.ipv6, ipv6)
 		}
 	}
 }
