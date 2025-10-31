67f9fa831adf625f7d8e78591936241f631ad35c
Konstantinos Karampogias <karampok@gmail.com>
2025-10-31 13:11:05 +0100

    Openshift Only increase wait-csv for downstream ci

--- openshift-ci/wait-for-csv.sh
+++ openshift-ci/wait-for-csv.sh
@@ -5,5 +5,5 @@ export VERSION="v0.0.0"
 export CSV_NAME="metallb-operator.${VERSION}"
 export NAMESPACE="metallb-system"
 
-timeout 5m bash -c 'until oc get csv -n $NAMESPACE $CSV_NAME; do sleep 5; done'
+timeout 6m bash -c 'until oc get csv -n $NAMESPACE $CSV_NAME; do sleep 5; done'
 oc wait --for jsonpath='{.status.phase}'=Succeeded csv/"$CSV_NAME" -n "$NAMESPACE" --timeout=300s
